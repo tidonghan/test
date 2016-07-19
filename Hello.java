269db75836510ead1bf39a77e4cd90d3b5cbbbf8
hantd <hantd@jointem.com>
2016-07-19 14:35:46 +0800

    hello print

--- Hello.java
+++ Hello.java
@@ -2,8 +2,8 @@ public class Hello
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("*******************");
-		System.out.println("###################");
+		System.out.println("hello brh");
+		System.out.println("hello world");
 	}
 
 	
