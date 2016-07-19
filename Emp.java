26e871c90f9def305a8fcd47591b8746d1708dc2
hantd <hantd@jointem.com>
2016-07-19 17:14:23 +0800

    add Users.java modifed Emp.java

--- Emp.java
+++ Emp.java
@@ -4,4 +4,6 @@ public class Emp implements Serializable
 	private Integer id;
 	private String name;
 	private String job;
+	private Double sal;
+	private Double common;
 }
---
+++ Users.java
@@ -0,0 +1,7 @@
+public class Users
+{
+	private Integer id;
+	private String userName;
+	private String password;
+	private Integer age;
+}
